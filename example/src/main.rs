a843ccba09e6bedde614d519bdf1e95617987e21
Mohammad Mustakim Ali <mustakimali@users.noreply.github.com>
2022-07-09 22:44:12 +0100

    rabbit as storage

--- example/src/main.rs
+++ example/src/main.rs
@@ -1,4 +1,7 @@
-use std::{sync::{Arc, Mutex, MutexGuard}, ops::{Deref, DerefMut}};
+use std::{
+    ops::{Deref, DerefMut},
+    sync::{Arc, Mutex, MutexGuard},
+};
 
 use fnf_rs::{storage::MemoryStorage, BackgroundJobServer};
 
@@ -31,22 +34,14 @@ impl<T> ArcMtx<T> {
 
 #[get("/")]
 fn hello(state: &State<Arc<Mutex<AppContext>>>) -> String {
+    let id = uuid::Uuid::new_v4().to_string();
     state
         .lock()
         .unwrap()
         .jobs
-        .enqueue("Enqueued job 1".to_string());
+        .enqueue(format!("Message: {}", id))
+        .expect("Enqueue Job");
     "Hello, world!".to_string()
-}
-
-#[get("/next")]
-fn next(state: &State<Arc<Mutex<AppContext>>>) -> String {
-    state
-        .lock()
-        .unwrap()
-        .jobs
-        .enqueue("Enqueued job 2".to_string());
-    "Hello, mo!!".to_string()
 }
 
 #[derive(Clone)]
@@ -66,9 +61,17 @@ pub fn handler(ctx: ArcMtx<JobContext>, input: String) -> anyhow::Result<()> {
 fn rocket() -> _ {
     let job_ctx = ArcMtx::new(JobContext {});
     let ms = fnf_rs::storage::MemoryStorage::new();
-    let bjs = BackgroundJobServer::start(ms, job_ctx, Box::new(handler));
+    let bjs = BackgroundJobServer::start(
+        "fnf-example",
+        "amqp://guest:guest@localhost:5672".into(),
+        ms,
+        job_ctx,
+        Box::new(handler),
+    )
+    .expect("start bg server");
 
     let ctx = Arc::new(Mutex::new(AppContext { jobs: bjs }));
 
-    rocket::build().mount("/", routes![hello, next]).manage(ctx)
+    let port = std::option_env!("PORT").unwrap_or_else(|| "8000");
+    rocket::build().mount("/", routes![hello]).manage(ctx)
 }
--- fnf-rs/src/lib.rs
+++ fnf-rs/src/lib.rs
@@ -1,5 +1,9 @@
-use std::sync::{Arc, Mutex};
+use std::{
+    sync::{Arc, Mutex},
+    thread::JoinHandle,
+};
 
+use amiquip::{Channel, Connection, ConsumerOptions, Exchange, Publish, QueueDeclareOptions};
 use storage::Storage;
 
 pub type HandlerFunc<C> = Box<dyn Fn(C, String) -> anyhow::Result<()> + Sync + Send>;
@@ -12,6 +16,11 @@ where
     storage: S,
     ctx: C,
     handler_fn: Arc<HandlerFunc<C>>,
+    connection: Connection,
+    channel: Channel,
+    routing_key: String,
+    workers: Vec<JoinHandle<anyhow::Result<()>>>,
+    // exchange: Exchange<'exchange>,
 }
 
 impl<S, C> BackgroundJobServer<S, C>
@@ -19,31 +28,95 @@ where
     S: Storage + Sync + Send,
     C: Sync + Send + Clone,
 {
-    pub fn start(storage: S, context: C, handler_fn: HandlerFunc<C>) -> Self {
-        Self {
+    pub fn start(
+        id: &str,
+        amqp_address: String,
+        storage: S,
+        context: C,
+        handler_fn: HandlerFunc<C>,
+    ) -> anyhow::Result<Self> {
+        let mut connection = Connection::insecure_open(&amqp_address)?;
+        let channel = connection.open_channel(None)?;
+        let routing_key = format!("fnf-rs-{}", id);
+        //exchange.publish(Publish::new("", routing_key))?;
+
+        let mut workers = Vec::new();
+        for id in 1..5 {
+            let amqp_address = amqp_address.clone();
+            let routing_key = routing_key.clone();
+
+            workers.push(std::thread::spawn(move || {
+                start_worker(id, &amqp_address, routing_key)
+            }));
+        }
+
+        Ok(Self {
             storage: storage,
             ctx: context,
             handler_fn: Arc::new(handler_fn),
-        }
+            // exchange,
+            channel,
+            connection,
+            routing_key,
+            workers,
+        })
     }
 
-    pub fn enqueue(&mut self, job: String) {
+    pub fn enqueue(&mut self, job: String) -> anyhow::Result<()> {
         let id = uuid::Uuid::new_v4().to_string();
         let handler = self.handler_fn.clone();
         let ctx = self.ctx.clone();
 
-        self.storage.set(format!("job-{}", id), job);
-        self.storage.push_job_id(id);
-        
+        // self.storage.set(format!("job-{}", id), job);
+        // self.storage.push_job_id(id);
+
+        let exchange = Exchange::direct(&&self.channel);
+        exchange.publish(Publish::new(job.as_bytes(), self.routing_key.clone()))?;
+
         // std::thread::spawn(move || {
         //     let r = (handler)(ctx, job);
         // });
 
         //let s = self.storage.get("".to_string());
+
+        Ok(())
     }
 }
 
-fn worker() {}
+fn start_worker(worker_id: i32, amqp_address: &str, routing_key: String) -> anyhow::Result<()> {
+    println!("[Worker#{}] Starting", worker_id);
+    let mut connection = Connection::insecure_open(&amqp_address)?;
+    let channel = connection.open_channel(None)?;
+    let queue = channel.queue_declare(
+        routing_key,
+        QueueDeclareOptions {
+            durable: true,
+            auto_delete: false,
+            ..Default::default()
+        },
+    )?;
+    let consumer = queue.consume(ConsumerOptions {
+        no_ack: false,
+        ..Default::default()
+    })?;
+
+    for (i, message) in consumer.receiver().iter().enumerate() {
+        match message {
+            amiquip::ConsumerMessage::Delivery(delivery) => {
+                let body = String::from_utf8_lossy(&delivery.body);
+                println!("[Worker#{}] ({:>3}) Received [{}]", worker_id, i, body);
+                consumer.ack(delivery)?;
+            }
+            other => {
+                println!("[Worker#{}] Consumer ended: {:?}", worker_id, other);
+                break;
+            }
+        }
+    }
+
+    println!("[Worker#{}] Ended", worker_id);
+    Ok(())
+}
 
 pub mod storage {
     use std::collections::{HashMap, HashSet};
