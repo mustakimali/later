d8bee65e80d1171960ff6ec9ccf26a3704ce62a0
Mohammad Mustakim Ali <mustakimali@users.noreply.github.com>
2022-08-04 17:09:59 +0100

    simplify

--- example/src/non_generated.rs
+++ example/src/non_generated.rs
@@ -1,4 +1,6 @@
 use later::{core::JobParameter, storage::redis::Redis, BackgroundJobServer, JobId};
+use macro_generated::*;
+use serde::{Deserialize, Serialize};
 
 struct AppContext {
     jobs: BackgroundJobServer<JobContext, DeriveHandler<JobContext>>,
@@ -67,259 +69,259 @@ pub struct AnotherSampleMessage {
 pub struct JobContext {}
 
 /* GENERATED */
+mod macro_generated {
+    use serde::{Deserialize, Serialize};
 
-use serde::{Deserialize, Serialize};
-
-pub struct DeriveHandlerContext<C: Send + Sync> {
-    inner: std::sync::Arc<DeriveHandlerContextInner<C>>,
-}
-impl<C> std::ops::Deref for DeriveHandlerContext<C>
-where
-    C: Send + Sync,
-{
-    type Target = DeriveHandlerContextInner<C>;
-
-    fn deref(&self) -> &Self::Target {
-        &self.inner
+    pub struct DeriveHandlerContext<C: Send + Sync> {
+        inner: std::sync::Arc<DeriveHandlerContextInner<C>>,
     }
-}
+    impl<C> std::ops::Deref for DeriveHandlerContext<C>
+    where
+        C: Send + Sync,
+    {
+        type Target = DeriveHandlerContextInner<C>;
 
-pub struct DeriveHandlerContextInner<C> {
-    job: ::later::BackgroundJobServerPublisher,
-    app: C,
-}
-
-impl<C> std::ops::Deref for DeriveHandlerContextInner<C> {
-    type Target = ::later::BackgroundJobServerPublisher;
-
-    fn deref(&self) -> &Self::Target {
-        &self.job
+        fn deref(&self) -> &Self::Target {
+            &self.inner
+        }
     }
-}
 
-pub struct DeriveHandlerBuilder<C>
-where
-    C: Sync + Send + 'static,
-{
-    ctx: C,
-    id: String,
-    amqp_address: String,
-    storage: Box<dyn ::later::storage::Storage>,
-    sample_message: ::core::option::Option<
-        Box<
-            dyn Fn(
-                    std::sync::Arc<DeriveHandlerContextInner<C>>,
-                    SampleMessage,
-                )
-                    -> ::later::futures::future::BoxFuture<'static, anyhow::Result<()>>
-                + Sync
-                + Send,
-        >,
-    >,
-    another_sample_message: ::core::option::Option<
-        Box<
-            dyn Fn(
-                    std::sync::Arc<DeriveHandlerContextInner<C>>,
-                    AnotherSampleMessage,
-                )
-                    -> ::later::futures::future::BoxFuture<'static, anyhow::Result<()>>
-                + Sync
-                + Send,
-        >,
-    >,
-}
+    pub struct DeriveHandlerContextInner<C> {
+        job: ::later::BackgroundJobServerPublisher,
+        app: C,
+    }
 
-impl<C> DeriveHandlerBuilder<C>
-where
-    C: Sync + Send + 'static,
-{
-    pub fn new(
-        context: C,
+    impl<C> std::ops::Deref for DeriveHandlerContextInner<C> {
+        type Target = ::later::BackgroundJobServerPublisher;
+
+        fn deref(&self) -> &Self::Target {
+            &self.job
+        }
+    }
+
+    pub struct DeriveHandlerBuilder<C>
+    where
+        C: Sync + Send + 'static,
+    {
+        ctx: C,
         id: String,
         amqp_address: String,
         storage: Box<dyn ::later::storage::Storage>,
-    ) -> Self
+        sample_message: ::core::option::Option<
+            Box<
+                dyn Fn(
+                        std::sync::Arc<DeriveHandlerContextInner<C>>,
+                        super::SampleMessage,
+                    )
+                        -> ::later::futures::future::BoxFuture<'static, anyhow::Result<()>>
+                    + Sync
+                    + Send,
+            >,
+        >,
+        another_sample_message: ::core::option::Option<
+            Box<
+                dyn Fn(
+                        std::sync::Arc<DeriveHandlerContextInner<C>>,
+                        super::AnotherSampleMessage,
+                    )
+                        -> ::later::futures::future::BoxFuture<'static, anyhow::Result<()>>
+                    + Sync
+                    + Send,
+            >,
+        >,
+    }
+
+    impl<C> DeriveHandlerBuilder<C>
     where
         C: Sync + Send + 'static,
     {
-        Self {
-            ctx: context,
-            id,
-            amqp_address,
-            storage,
+        pub fn new(
+            context: C,
+            id: String,
+            amqp_address: String,
+            storage: Box<dyn ::later::storage::Storage>,
+        ) -> Self
+        where
+            C: Sync + Send + 'static,
+        {
+            Self {
+                ctx: context,
+                id,
+                amqp_address,
+                storage,
 
-            sample_message: ::core::option::Option::None,
-            another_sample_message: ::core::option::Option::None,
+                sample_message: ::core::option::Option::None,
+                another_sample_message: ::core::option::Option::None,
+            }
         }
-    }
 
-    /// Accept a simplified and ergonomic async function handler
-    ///     Fn(Ctx<C>, Payload) -> impl Future<Output = anyhow::Result<()>>
-    /// and map this to the complex/nasty stuff required internally to make the compiler happy.
-    ///     Fn(Arc<CtxWrapper<C>>, Payload) -> Pin<Box<Future<Output = anyhow::Result<()>>>
-    fn wrap_complex_handler<Payload, HandlerFunc, Fut>(
-        arc_ctx: std::sync::Arc<DeriveHandlerContextInner<C>>,
-        payload: Payload,
-        handler: HandlerFunc,
-    ) -> ::later::futures::future::BoxFuture<'static, Fut::Output>
-    where
-        HandlerFunc: Fn(DeriveHandlerContext<C>, Payload) -> Fut + Send + 'static,
-        Payload: ::later::core::JobParameter + Send + 'static,
-        Fut: ::later::futures::future::Future<Output = anyhow::Result<()>> + Send,
-    {
-        Box::pin(async move {
-            let ctx = DeriveHandlerContext {
-                inner: arc_ctx.clone(),
+        /// Accept a simplified and ergonomic async function handler
+        ///     Fn(Ctx<C>, Payload) -> impl Future<Output = anyhow::Result<()>>
+        /// and map this to the complex/nasty stuff required internally to make the compiler happy.
+        ///     Fn(Arc<CtxWrapper<C>>, Payload) -> Pin<Box<Future<Output = anyhow::Result<()>>>
+        fn wrap_complex_handler<Payload, HandlerFunc, Fut>(
+            arc_ctx: std::sync::Arc<DeriveHandlerContextInner<C>>,
+            payload: Payload,
+            handler: HandlerFunc,
+        ) -> ::later::futures::future::BoxFuture<'static, Fut::Output>
+        where
+            HandlerFunc: Fn(DeriveHandlerContext<C>, Payload) -> Fut + Send + 'static,
+            Payload: ::later::core::JobParameter + Send + 'static,
+            Fut: ::later::futures::future::Future<Output = anyhow::Result<()>> + Send,
+        {
+            Box::pin(async move {
+                let ctx = DeriveHandlerContext {
+                    inner: arc_ctx.clone(),
+                };
+                let fut = handler(ctx, payload);
+                fut.await
+            })
+        }
+
+        ///Register a handler for [`SampleMessage`].
+        ///This handler will be called when a job is enqueued with a payload of this type.
+        pub fn with_sample_message_handler<M, Fut>(mut self, handler: M) -> Self
+        where
+            M: Fn(DeriveHandlerContext<C>, super::SampleMessage) -> Fut
+                + Send
+                + Sync
+                + Copy
+                + 'static,
+            Fut: ::later::futures::future::Future<Output = anyhow::Result<()>> + Send,
+            C: Sync + Send + 'static,
+        {
+            self.sample_message = Some(Box::new(move |ctx, payload| {
+                Self::wrap_complex_handler(ctx, payload, handler)
+            }));
+            self
+        }
+
+        ///Register a handler for [`AnotherSampleMessage`].
+        ///This handler will be called when a job is enqueued with a payload of this type.
+        pub fn with_another_sample_message_handler<M, Fut>(mut self, handler: M) -> Self
+        where
+            M: Fn(DeriveHandlerContext<C>, super::AnotherSampleMessage) -> Fut
+                + Send
+                + Sync
+                + Copy
+                + 'static,
+            Fut: ::later::futures::future::Future<Output = anyhow::Result<()>> + Send,
+            C: Sync + Send + 'static,
+        {
+            self.another_sample_message = Some(Box::new(move |ctx, payload| {
+                Self::wrap_complex_handler(ctx, payload, handler)
+            }));
+            self
+        }
+
+        pub fn build(self) -> anyhow::Result<::later::BackgroundJobServer<C, DeriveHandler<C>>> {
+            let publisher = ::later::BackgroundJobServerPublisher::new(
+                self.id.clone(),
+                self.amqp_address.clone(),
+                self.storage,
+            )?;
+            let ctx = DeriveHandlerContextInner {
+                job: publisher,
+                app: self.ctx,
             };
-            let fut = handler(ctx, payload);
-            fut.await
-        })
-    }
+            let handler = DeriveHandler {
+                ctx: std::sync::Arc::new(ctx),
+                sample_message: self.sample_message,
+                another_sample_message: self.another_sample_message,
+            };
 
-    ///Register a handler for [`SampleMessage`].
-    ///This handler will be called when a job is enqueued with a payload of this type.
-    pub fn with_sample_message_handler<M, Fut>(mut self, handler: M) -> Self
-    where
-        M: Fn(DeriveHandlerContext<C>, SampleMessage) -> Fut + Send + Sync + Copy + 'static,
-        Fut: ::later::futures::future::Future<Output = anyhow::Result<()>> + Send,
-        C: Sync + Send + 'static,
-    {
-        self.sample_message = Some(Box::new(move |ctx, payload| {
-            Self::wrap_complex_handler(ctx, payload, handler)
-        }));
-        self
-    }
-
-    ///Register a handler for [`AnotherSampleMessage`].
-    ///This handler will be called when a job is enqueued with a payload of this type.
-    pub fn with_another_sample_message_handler<M, Fut>(mut self, handler: M) -> Self
-    where
-        M: Fn(DeriveHandlerContext<C>, AnotherSampleMessage) -> Fut + Send + Sync + Copy + 'static,
-        Fut: ::later::futures::future::Future<Output = anyhow::Result<()>> + Send,
-        C: Sync + Send + 'static,
-    {
-        self.another_sample_message = Some(Box::new(move |ctx, payload| {
-            Self::wrap_complex_handler(ctx, payload, handler)
-        }));
-        self
-    }
-    // where
-    //     M: Fn(std::sync::Arc<DeriveHandlerContextInner<C>>, AnotherSampleMessage) -> PinnedBoxFut
-    //         + Send
-    //         + Sync
-    //         + 'static,
-    //     C: Sync + Send + 'static,
-    // {
-    //     self.another_sample_message = Some(Box::new(handler));
-    //     self
-    // }
-
-    pub fn build(self) -> anyhow::Result<BackgroundJobServer<C, DeriveHandler<C>>> {
-        let publisher = ::later::BackgroundJobServerPublisher::new(
-            self.id.clone(),
-            self.amqp_address.clone(),
-            self.storage,
-        )?;
-        let ctx = DeriveHandlerContextInner {
-            job: publisher,
-            app: self.ctx,
-        };
-        let handler = DeriveHandler {
-            ctx: std::sync::Arc::new(ctx),
-            sample_message: self.sample_message,
-            another_sample_message: self.another_sample_message,
-        };
-
-        BackgroundJobServer::start(handler)
-    }
-}
-impl ::later::core::JobParameter for SampleMessage {
-    fn to_bytes(&self) -> anyhow::Result<Vec<u8>> {
-        let result = ::later::encoder::encode(&self);
-        let result = ::later::anyhow::Context::context(result, "unable to serialize");
-        Ok(result?)
-    }
-    fn from_bytes(payload: &[u8]) -> Self {
-        ::later::encoder::decode(payload).unwrap()
-    }
-    fn get_ptype(&self) -> String {
-        "sample_message".into()
-    }
-}
-impl ::later::core::JobParameter for AnotherSampleMessage {
-    fn to_bytes(&self) -> anyhow::Result<Vec<u8>> {
-        let result = ::later::encoder::encode(&self);
-        let result = ::later::anyhow::Context::context(result, "unable to serialize");
-        Ok(result?)
-    }
-    fn from_bytes(payload: &[u8]) -> Self {
-        ::later::encoder::decode(payload).unwrap()
-    }
-    fn get_ptype(&self) -> String {
-        "another_sample_message".into()
-    }
-}
-
-pub struct DeriveHandler<C>
-where
-    C: Sync + Send + 'static,
-{
-    pub ctx: std::sync::Arc<DeriveHandlerContextInner<C>>,
-    pub sample_message: ::core::option::Option<
-        Box<
-            dyn Fn(
-                    std::sync::Arc<DeriveHandlerContextInner<C>>,
-                    SampleMessage,
-                )
-                    -> ::later::futures::future::BoxFuture<'static, anyhow::Result<()>>
-                + Send
-                + Sync,
-        >,
-    >,
-    pub another_sample_message: ::core::option::Option<
-        Box<
-            dyn Fn(
-                    std::sync::Arc<DeriveHandlerContextInner<C>>,
-                    AnotherSampleMessage,
-                )
-                    -> ::later::futures::future::BoxFuture<'static, anyhow::Result<()>>
-                + Send
-                + Sync,
-        >,
-    >,
-}
-
-#[async_trait]
-impl<C> ::later::core::BgJobHandler<C> for DeriveHandler<C>
-where
-    C: Sync + Send + 'static,
-{
-    async fn dispatch(&self, ptype: String, payload: &[u8]) -> anyhow::Result<()> {
-        match ptype.as_str() {
-            "sample_message" => {
-                let payload = SampleMessage::from_bytes(payload);
-                if let Some(handler) = &self.sample_message {
-                    (handler)(self.ctx.clone(), payload).await
-                } else {
-                    unimplemented!()
-                }
-            }
-            "another_sample_message" => {
-                let payload = AnotherSampleMessage::from_bytes(payload);
-                if let Some(handler) = &self.another_sample_message {
-                    (handler)(self.ctx.clone(), payload).await
-                } else {
-                    unimplemented!()
-                }
-            }
-            _ => unimplemented!(),
+            ::later::BackgroundJobServer::start(handler)
         }
     }
-    fn get_ctx(&self) -> &C {
-        &self.ctx.app
+    impl ::later::core::JobParameter for super::SampleMessage {
+        fn to_bytes(&self) -> anyhow::Result<Vec<u8>> {
+            let result = ::later::encoder::encode(&self);
+            let result = ::later::anyhow::Context::context(result, "unable to serialize");
+            Ok(result?)
+        }
+        fn from_bytes(payload: &[u8]) -> Self {
+            ::later::encoder::decode(payload).unwrap()
+        }
+        fn get_ptype(&self) -> String {
+            "sample_message".into()
+        }
+    }
+    impl ::later::core::JobParameter for super::AnotherSampleMessage {
+        fn to_bytes(&self) -> anyhow::Result<Vec<u8>> {
+            let result = ::later::encoder::encode(&self);
+            let result = ::later::anyhow::Context::context(result, "unable to serialize");
+            Ok(result?)
+        }
+        fn from_bytes(payload: &[u8]) -> Self {
+            ::later::encoder::decode(payload).unwrap()
+        }
+        fn get_ptype(&self) -> String {
+            "another_sample_message".into()
+        }
     }
 
-    fn get_publisher(&self) -> &::later::BackgroundJobServerPublisher {
-        &self.ctx.job
+    pub struct DeriveHandler<C>
+    where
+        C: Sync + Send + 'static,
+    {
+        pub ctx: std::sync::Arc<DeriveHandlerContextInner<C>>,
+        pub sample_message: ::core::option::Option<
+            Box<
+                dyn Fn(
+                        std::sync::Arc<DeriveHandlerContextInner<C>>,
+                        super::SampleMessage,
+                    )
+                        -> ::later::futures::future::BoxFuture<'static, anyhow::Result<()>>
+                    + Send
+                    + Sync,
+            >,
+        >,
+        pub another_sample_message: ::core::option::Option<
+            Box<
+                dyn Fn(
+                        std::sync::Arc<DeriveHandlerContextInner<C>>,
+                        super::AnotherSampleMessage,
+                    )
+                        -> ::later::futures::future::BoxFuture<'static, anyhow::Result<()>>
+                    + Send
+                    + Sync,
+            >,
+        >,
+    }
+
+    #[async_trait]
+    impl<C> ::later::core::BgJobHandler<C> for DeriveHandler<C>
+    where
+        C: Sync + Send + 'static,
+    {
+        async fn dispatch(&self, ptype: String, payload: &[u8]) -> anyhow::Result<()> {
+            use later::core::JobParameter;
+            match ptype.as_str() {
+                "sample_message" => {
+                    let payload = super::SampleMessage::from_bytes(payload);
+                    if let Some(handler) = &self.sample_message {
+                        (handler)(self.ctx.clone(), payload).await
+                    } else {
+                        unimplemented!()
+                    }
+                }
+                "another_sample_message" => {
+                    let payload = super::AnotherSampleMessage::from_bytes(payload);
+                    if let Some(handler) = &self.another_sample_message {
+                        (handler)(self.ctx.clone(), payload).await
+                    } else {
+                        unimplemented!()
+                    }
+                }
+                _ => unimplemented!(),
+            }
+        }
+        fn get_ctx(&self) -> &C {
+            &self.ctx.app
+        }
+
+        fn get_publisher(&self) -> &::later::BackgroundJobServerPublisher {
+            &self.ctx.job
+        }
     }
 }
